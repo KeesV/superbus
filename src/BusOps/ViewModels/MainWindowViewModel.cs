fbe602a9f076186e751bb543f0b30739b09b4870
Kees Verhaar <kees.verhaar@wigo4it.nl>
2025-10-09 17:21:20 +0200

    🎨 Add some sample data for navigation menu

--- src/BusOps/ViewModels/MainWindowViewModel.cs
+++ src/BusOps/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,57 @@
 using ReactiveUI;
 using System.Reactive;
-using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using BusOps.Core.Interfaces;
 using BusOps.Core.Models;
+using DynamicData;
 using Microsoft.Extensions.Logging;
 
 namespace BusOps.ViewModels;
+
+public class DesignMainWindowViewModel : MainWindowViewModel
+{
+    public DesignMainWindowViewModel() : base(null!, null!, null!, null!)
+    {
+        Connections.AddRange([
+            new ConnectionItemViewModel(new ServiceBusConnection
+            {
+                CreatedAt = DateTimeOffset.Now,
+                Description = "sb-013-hub-nonprod",
+                Id = "sb-013-hub-nonprod",
+                IsActive = true,
+                LastConnected = DateTimeOffset.Now.AddHours(-1),
+            }, this)
+        ]);
+        Entities.AddRange([
+            new EntityTreeItemViewModel
+            {
+                Name = "Queues",
+                Type = "Folder",
+                MessageCount = 79,
+            }
+        ]);
+        Entities[0].Children.AddRange([
+            new EntityTreeItemViewModel
+            {
+                Name = "my-queue-1",
+                Type = "Queue",
+                MessageCount = 42,
+            },
+            new EntityTreeItemViewModel
+            {
+                Name = "my-queue-2",
+                Type = "Queue",
+                MessageCount = 0,
+            },
+            new EntityTreeItemViewModel
+            {
+                Name = "my-queue-3",
+                Type = "Queue",
+                MessageCount = 37,
+            }
+        ]);
+    }
+}
 
 public class MainWindowViewModel : ReactiveObject
 {
